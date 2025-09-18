9adce4283978b2c9e2c3215385365ae14b2d4fa9
Abhineetsahay <abhineetsahay@gmail.com>
2025-09-19 00:13:36 +0530

    update

--- main.py
+++ main.py
@@ -218,6 +218,11 @@ def upload_and_predict():
 
 
 
+@app.route("/")
+def home():
+    return "Hello, Flask on Render!"
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
